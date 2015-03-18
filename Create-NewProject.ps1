415be09d43ecfe56ce93e04568221f2510a6ffea
Emmanuel Brandão <egomesbrandao@gmail.com>
2015-03-18 00:08:42 -0300

    Added the full code from https://gist.github.com/egomesbrandao/aea33e27c112225051d7

--- Create-NewProject.ps1
+++ Create-NewProject.ps1
@@ -1,3 +1,10 @@
+Param(
+    [parameter(Mandatory=$false)][string] $CollectionUrlParam = $(Read-Host -prompt "Collection URL"),
+    [parameter(Mandatory=$false)][string] $TeamProjectParam = $(Read-Host -prompt "Team Project"),
+	[parameter(Mandatory=$true)][String] $GlobalListName = $(Read-Host -prompt "Project List"),
+    [parameter(Mandatory=$true)][String] $GlobalEntryValue = $(Read-Host -prompt "New project name")
+    )
+
 $pathToAss2 = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\ReferenceAssemblies\v2.0"
 $pathToAss4 = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\ReferenceAssemblies\v4.5"
 Add-Type -Path "$pathToAss2\Microsoft.TeamFoundation.Client.dll"
@@ -70,3 +77,78 @@ function Get-TfsTeamSettingsConfigurationService {
         )
     return $TfsCollection.GetService([ Microsoft.TeamFoundation.ProcessConfiguration.Client.TeamSettingsConfigurationService]);
 }
+
+function Add-TfsGlobalListItem {
+    Param(
+        [parameter(Mandatory=$true)][Microsoft.TeamFoundation.Client.TfsTeamProjectCollection] $TfsCollection,
+        [parameter(Mandatory=$true)][String] $GlobalListName,
+        [parameter(Mandatory=$true)][String] $GlobalEntryValue
+        )
+    # Get Global List
+    $store = Get-TfsWorkItemStore $TfsCollection
+    [xml]$export = $store.ExportGlobalLists();
+
+    $globalLists = $export.ChildNodes[0];
+    $globalList = $globalLists.SelectSingleNode("//GLOBALLIST[@name='$GlobalListName']")
+
+    # if no GL then add it
+    If ($globalList -eq $null)
+    {
+        $globalList = $export.CreateElement("GLOBALLIST");
+        $globalListNameAttribute = $export.CreateAttribute("name");
+        $globalListNameAttribute.Value = $GlobalListName
+        $globalList.Attributes.Append($globalListNameAttribute);
+        $globalLists.AppendChild($globalList);
+    }
+
+    #Create a new node.
+    $GlobalEntry = $export.CreateElement("LISTITEM");
+    $GlobalEntryAttribute = $export.CreateAttribute("value");
+    $GlobalEntryAttribute.Value = $GlobalEntryValue
+    $GlobalEntry.Attributes.Append($GlobalEntryAttribute);
+
+    #Add new entry to list
+    $globalList.AppendChild($GlobalEntry)
+    # Import list to server
+    $store.ImportGlobalLists($globalLists)
+}
+
+$TfsCollection = Get-TfsCollection $CollectionUrlParam
+
+$TfsCollection.EnsureAuthenticated()
+
+$store = Get-TfsWorkItemStore $TfsCollection
+
+#If the list does not exist creates or just add the name of the new project
+[xml] $export = $store.ExportGlobalLists();
+
+$globalLists = $export.ChildNodes[0];
+
+$globalList = $globalLists.SelectSingleNode("//GLOBALLIST[@name='$GlobalListName']")
+
+If ($globalList -eq $null)
+{
+    $globalList = $export.CreateElement("GLOBALLIST");
+    $globalListNameAttribute = $export.CreateAttribute("name");
+    $globalListNameAttribute.Value = $GlobalListName
+    $globalList.Attributes.Append($globalListNameAttribute);
+    $globalLists.AppendChild($globalList);
+}
+
+$GlobalEntry = $export.CreateElement("LISTITEM");
+$GlobalEntryAttribute = $export.CreateAttribute("value");
+$GlobalEntryAttribute.Value = $GlobalEntryValue
+$GlobalEntry.Attributes.Append($GlobalEntryAttribute);
+
+$globalList.AppendChild($GlobalEntry)
+
+$store.ImportGlobalLists($globalLists)
+
+#Creates Team with the new name of the Project
+$cssService = $TfsCollection.GetService("Microsoft.TeamFoundation.Server.ICommonStructureService3")
+
+$TeamProject += $cssService.GetProjectFromName($TeamProjectParam)
+
+$teamService = $TfsCollection.GetService("Microsoft.TeamFoundation.Client.TfsTeamService")
+
+$Team = $teamService.CreateTeam($teamProject.Uri, $GlobalEntryValue, "", $null)
